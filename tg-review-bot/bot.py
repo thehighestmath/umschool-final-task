0e9f5d939415c7fc805d18b721ef77c80082cec4
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-04-07 13:27:44 +0300

    f

--- tg-review-bot/bot.py
+++ tg-review-bot/bot.py
@@ -5,18 +5,26 @@ import logging
 import telebot.formatting
 
 from code_review_checker import CodeReviewChecker
+from config import TOKEN
 
-logging.basicConfig(filename='bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    filename="bot.log",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
 
-# Ваш токен бота
-TOKEN = '7120003879:AAG34pRyUHmT0w06EoXzF8jpTjAJoPtnadg'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Отправь мне ссылку на проект на GitHub, который нужно проверить.')
+    bot.send_message(
+        message.chat.id,
+        "Привет! Отправь мне ссылку на проект на GitHub, который нужно проверить.",
+    )
     logging.info("Бот запущен, начало работы с пользователем.")
+
 
 @bot.message_handler(func=lambda message: True)
 def check_project(message):
@@ -25,32 +33,34 @@ def check_project(message):
     bot.send_message(
         chat_id,
         telebot.formatting.format_text(
-            telebot.formatting.hbold('Проверка началась.'),
-            f'Проект {project_url}'
+            telebot.formatting.hbold("Проверка началась."), f"Проект {project_url}"
         ),
-        parse_mode='HTML'
+        parse_mode="HTML",
     )
-    os.system(f'git clone {project_url} temp_repo')
+    os.system(f"git clone {project_url} temp_repo")
 
-    checker = CodeReviewChecker('temp_repo')
+    checker = CodeReviewChecker("temp_repo")
     checker.run_checks()
     messages = checker.messages
 
-    os.system('rm -rf temp_repo')
+    os.system("rm -rf temp_repo")
 
-    result = '\n'.join(messages)
-    verdict = result or 'Бот не нашёл замечаний'
+    result = "\n".join(messages)
+    verdict = result or "Бот не нашёл замечаний"
     bot.send_message(
         chat_id,
         telebot.formatting.format_text(
-            telebot.formatting.hbold('Проверка закончена.'),
-            'Если есть возражения -- писать @the_real_math / @kiril_python',
-            telebot.formatting.hitalic('beta. v0.1'),
-            '',
+            telebot.formatting.hbold("Проверка закончена."),
+            "Если есть возражения -- писать @the_real_math / @kiril_python",
+            telebot.formatting.hitalic("beta. v0.1"),
+            "",
             telebot.formatting.hcode(verdict),
         ),
-        parse_mode='HTML'
+        parse_mode="HTML",
     )
-    logging.info(f"Проверка проекта {project_url} завершена. Результат отправлен пользователю. {verdict}")
+    logging.info(
+        f"Проверка проекта {project_url} завершена. Результат отправлен пользователю. {verdict}"
+    )
+
 
 bot.polling()
