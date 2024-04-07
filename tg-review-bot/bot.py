4cf75e8b52e80b7a5f08a05cd64ca7563a2bdccc
Kirill Kryzhanovsky <42893255+thehighestmath@users.noreply.github.com>
2024-04-07 13:34:27 +0300

    Bot (#3)

---
+++ tg-review-bot/bot.py
@@ -0,0 +1,67 @@
+import telebot
+import os
+import logging
+
+import telebot.formatting
+
+from code_review_checker import CodeReviewChecker
+from config import TOKEN
+
+logging.basicConfig(
+    filename="bot.log",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Отправь мне ссылку на проект на GitHub, который нужно проверить.",
+    )
+    logging.info("Бот запущен, начало работы с пользователем.")
+
+
+@bot.message_handler(func=lambda message: True)
+def check_project(message):
+    chat_id = message.chat.id
+    project_url = message.text
+    bot.send_message(
+        chat_id,
+        telebot.formatting.format_text(
+            telebot.formatting.hbold("Проверка началась."), f"Проект {project_url}"
+        ),
+        parse_mode="HTML",
+    )
+    os.system(f"git clone {project_url} temp_repo")
+
+    checker = CodeReviewChecker("temp_repo")
+    checker.run_checks()
+    messages = checker.messages
+
+    os.system("rm -rf temp_repo")
+
+    result = "\n".join(messages)
+    verdict = result or "Бот не нашёл замечаний"
+    bot.send_message(
+        chat_id,
+        telebot.formatting.format_text(
+            telebot.formatting.hbold("Проверка закончена."),
+            "Если есть возражения -- писать @the_real_math / @kiril_python",
+            "Рекомендую почитать этот документ даже если бот НЕ нашёл замечаний",
+            telebot.formatting.hitalic("beta. v0.1"),
+            "",
+            telebot.formatting.hcode(verdict),
+        ),
+        parse_mode="HTML",
+    )
+    logging.info(
+        f"Проверка проекта {project_url} завершена. Результат отправлен пользователю. {verdict}"
+    )
+
+
+bot.polling()
---
+++ tg-review-bot/code_review_checker.py
@@ -0,0 +1,63 @@
+import os
+import glob
+
+
+class CodeReviewChecker:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.messages = []
+
+    def load_code(self, filename):
+        with open(filename, "r") as file:
+            return file.readlines()
+
+    def check_concatenation_in_loop(self, code, filename):
+        for i, line in enumerate(code):
+            if "for" in line:
+                for j in range(
+                    i, min(i + 3, len(code))
+                ):
+                    next_line = code[j].strip()
+                    if "+" in next_line:
+                        self.messages.append(
+                            f"Обнаружено склеивание строк в цикле в строке {j+1} файла {filename}: {next_line}"
+                        )
+                        break
+
+    def check_connection_duplication(self, code, filename):
+        connect_count = 0
+        for i, line in enumerate(code):
+            if "connect(" in line:
+                connect_count += 1
+                if connect_count > 1:
+                    self.messages.append(
+                        f"Обнаружено дублирование подключения к базе данных в строке {i+1} файла {filename}: {line.strip()}"
+                    )
+                    break
+
+    def check_f_strings(self, code, filename):
+        for i, line in enumerate(code):
+            if "%" in line:
+                self.messages.append(
+                    f"Обнаружено использование старого форматирования строк в строке {i+1} файла {filename}: {line.strip()}"
+                )
+
+    def check_backslash_continuations(self, code, filename):
+        for i, line in enumerate(code):
+            if "\\" in line and line.strip().endswith("\\"):
+                self.messages.append(
+                    f"Обнаружены продолжения строк с использованием обратного слэша в строке {i+1} файла {filename}: {line.strip()}"
+                )
+
+    def run_checks(self):
+        file_list = glob.glob(os.path.join(self.folder_path, "**/*.py"), recursive=True)
+
+        # Выводим список файлов
+        for file_path in file_list:
+            if file_path.endswith(".py"):
+                code = self.load_code(file_path)
+                self.check_concatenation_in_loop(code, file_path)
+                self.check_connection_duplication(code, file_path)
+                self.check_f_strings(code, file_path)
+                self.check_backslash_continuations(code, file_path)
+        return self.messages
